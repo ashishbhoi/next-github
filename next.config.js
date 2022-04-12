11199ea20564d96ba722ee46057497a186571a2f
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2022-04-13 03:55:53 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === "production";
+// const nextConfig = {
+//   reactStrictMode: true
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  assetPrefix: isProd ? "/next-github/" : ""
+}
